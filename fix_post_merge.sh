937f6908c4c2b40b3a0407e72d6e69b0be109fb4
daishiman <daishimanju@gmail.com>
2025-04-27 15:04:52 +0900

    fix: パフォーマンステストのタイムアウト設定を延長

--- fix_post_merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# マイグレーションファイルのパターンを修正する
-sed -i "" "s/migrations\\|schema.sql\\|db\\/schema/migrations\\|schema.sql\\|db\\/schema\\|migrate.js\\|database-migration/g" .husky/post-merge
-
-echo "post-mergeスクリプトのパターンを更新しました"
